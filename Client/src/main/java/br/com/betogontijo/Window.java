cf6752fb1a5b5e1ac2630420678c2d634a9ee9b9
886390 <886390@PMG34INFLB20709.PUCMINAS.NET>
2018-03-07 16:34:20 -0300

    ...

--- Client/src/main/java/br/com/betogontijo/Window.java
+++ Client/src/main/java/br/com/betogontijo/Window.java
@@ -1,5 +1,6 @@
 package br.com.betogontijo;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import java.util.Set;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.knowm.xchange.dto.marketdata.Ticker;
@@ -27,28 +28,76 @@ public class Window extends JFrame {
 	 */
 	private static final long serialVersionUID = -7321835299704305072L;
 
+	final static String NULL_VALUE = "-";
+
 	JComboBox<String> key = new JComboBox<String>();
 	static JComboBox<String> value = new JComboBox<String>();
 	static DistributedService service;
+	final static JLabel curLabel = new JLabel("Valor Atual: ");
+	JLabel curValue = new JLabel(NULL_VALUE);
+	final static JLabel lowLabel = new JLabel("Menor Valor: ");
+	JLabel lowValue = new JLabel(NULL_VALUE);
+	final static JLabel highLabel = new JLabel("Maior Valor: ");
+	JLabel highValue = new JLabel(NULL_VALUE);
+	final static JLabel avgLabel = new JLabel("Valor Médio: ");
+	JLabel avgValue = new JLabel(NULL_VALUE);
+	final static JLabel timestampLabel = new JLabel("Horário: ");
+	JLabel timestampValue = new JLabel(NULL_VALUE);
+	final static JLabel volumeLabel = new JLabel("Volume: ");
+	JLabel volumeValue = new JLabel(NULL_VALUE);
 
 	public Window() {
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(ClientApplication.class,
 				new String[] {});
 		service = applicationContext.getBean(DistributedService.class);
 		setTitle("Client");
-		JPanel jPanel = new JPanel();
-		jPanel.add(key);
-		jPanel.add(value);
+		JPanel jPanel = new JPanel(new GridLayout(10, 1));
+		JPanel keyValue = new JPanel(new GridLayout(1, 2));
+		keyValue.add(key);
+		keyValue.add(value);
+		jPanel.add(keyValue);
 		JButton jButton = new JButton("Retrieve");
 		jButton.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Ticker convert = service.convert(key.getSelectedItem().toString(), value.getSelectedItem().toString());
-				JOptionPane.showMessageDialog(Window.this, convert.toString());
+
+				curValue.setText(convert.getLast() == null ? NULL_VALUE : convert.getLast() + "");
+				lowValue.setText(convert.getLow() == null ? NULL_VALUE : convert.getLow() + "");
+				highValue.setText(convert.getHigh() == null ? NULL_VALUE : convert.getHigh() + "");
+				avgValue.setText(convert.getVwap() == null ? NULL_VALUE : convert.getVwap() + "");
+				timestampValue.setText(convert.getTimestamp() == null ? NULL_VALUE : convert.getTimestamp() + "");
+				volumeValue.setText(convert.getVolume() == null ? NULL_VALUE : convert.getVolume() + "");
+				// JOptionPane.showMessageDialog(Window.this, convert.toString());
 			}
 		});
-		jPanel.add(jButton);
+		keyValue.add(jButton);
+		jPanel.add(keyValue);
+		JPanel curPanel = new JPanel(new GridLayout(1, 2));
+		curPanel.add(curLabel);
+		curPanel.add(curValue);
+		JPanel lowPanel = new JPanel(new GridLayout(1, 2));
+		lowPanel.add(lowLabel);
+		lowPanel.add(lowValue);
+		JPanel highPanel = new JPanel(new GridLayout(1, 2));
+		highPanel.add(highLabel);
+		highPanel.add(highValue);
+		JPanel avgPanel = new JPanel(new GridLayout(1, 2));
+		avgPanel.add(avgLabel);
+		avgPanel.add(avgValue);
+		JPanel timestampPanel = new JPanel(new GridLayout(1, 2));
+		timestampPanel.add(timestampLabel);
+		timestampPanel.add(timestampValue);
+		JPanel volumePanel = new JPanel(new GridLayout(1, 2));
+		volumePanel.add(volumeLabel);
+		volumePanel.add(volumeValue);
+		jPanel.add(curPanel);
+		jPanel.add(lowPanel);
+		jPanel.add(highPanel);
+		jPanel.add(avgPanel);
+		jPanel.add(timestampPanel);
+		jPanel.add(volumePanel);
+
 		add(jPanel);
 		setSize(500, 500);
 		setVisible(true);
@@ -65,12 +114,17 @@ public class Window extends JFrame {
 		key.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				Window.value.removeAllItems();
 				for (String value : map.get(key.getSelectedItem())) {
 					Window.value.addItem(value);
 					Window.value.setEnabled(true);
 				}
 			}
 		});
+		for (String value : map.get(key.getSelectedItem())) {
+			Window.value.addItem(value);
+			Window.value.setEnabled(true);
+		}
 		// Ticker ticker = service.convert("BTC", "USD");
 		// System.out.println(ticker);
 	}
